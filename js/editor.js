7c503ceb17f759d7c6d76b9be06828f4089e62ae
Gabriele Fronzé <sucre.91@hotmail.it>
2025-10-14 12:31:13 +0200

    Move matomo to head

--- js/editor.js
+++ js/editor.js
@@ -24,6 +24,21 @@ document.addEventListener('DOMContentLoaded', function() {
   <link rel='stylesheet' href='../css/style.css'>
   <link rel='stylesheet' href='../css/themes.css'>
   <link rel="icon" href="../favicon.ico">
+    <!-- Matomo -->
+    <script>
+        var _paq = window._paq = window._paq || [];
+        /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+        _paq.push(['trackPageView']);
+        _paq.push(['enableLinkTracking']);
+        (function() {
+        var u="//matomo.elemento.cloud/";
+        _paq.push(['setTrackerUrl', u+'matomo.php']);
+        _paq.push(['setSiteId', '1']);
+        var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+        g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+        })();
+    </script>
+    <!-- End Matomo Code -->
 </head>
 <body class='theme-default'>
   <!-- Background Canvas for animated gradients -->
